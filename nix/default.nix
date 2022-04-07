976cd06b3f1699dabe0aa3b79796705fa71c0966
a-kenji <aks.kenji@protonmail.com>
2022-04-07 08:41:02 +0200

    add: overlays for the packages

--- nix/default.nix
+++ nix/default.nix
@@ -14,97 +14,108 @@ flake-utils.lib.eachSystem [
   "x86_64-darwin"
   "x86_64-linux"
 ]
-(system: let
-  overlays = [(import rust-overlay)];
+(
+  system: let
+    overlays = [(import rust-overlay)];
 
-  pkgs = import nixpkgs {inherit system overlays;};
+    pkgs = import nixpkgs {inherit system overlays;};
 
-  crate2nixPkgs = import nixpkgs {
-    inherit system;
-    overlays = [
-      (self: _: {
-        rustc = rustToolchainToml;
-        cargo = rustToolchainToml;
-      })
+    crate2nixPkgs = import nixpkgs {
+      inherit system;
+      overlays = [
+        (self: _: {
+          rustc = rustToolchainToml;
+          cargo = rustToolchainToml;
+        })
+      ];
+    };
+
+    name = "conduit";
+    pname = name;
+    root = conduit;
+
+    ignoreSource = [".git" "target" "example"];
+
+    src = pkgs.nix-gitignore.gitignoreSource ignoreSource root;
+
+    rustToolchainToml = pkgs.rust-bin.fromRustupToolchainFile ../rust-toolchain.toml;
+    cargoLockFile = conduit + /Cargo.lock;
+
+    cargo = rustToolchainToml;
+    rustc = rustToolchainToml;
+
+    buildInputs = [
+      pkgs.rocksdb
     ];
-  };
 
-  name = "conduit";
-  pname = name;
-  root = conduit;
+    nativeBuildInputs = [
+      pkgs.rustPlatform.bindgenHook
+    ];
 
-  ignoreSource = [".git" "target" "example"];
+    devInputs = [
+      rustToolchainToml
+    ];
 
-  src = pkgs.nix-gitignore.gitignoreSource ignoreSource root;
+    fmtInputs = [
+      pkgs.alejandra
+      pkgs.treefmt
+    ];
+    # TODO add meta
+  in rec {
+    # crate2nix - better incremental builds, but uses ifd
+    #packages.conduit = crate2nixPkgs.callPackage ./crate2nix.nix {
+    #inherit
+    #name
+    #src
+    #crate2nix
+    #;
+    #};
 
-  rustToolchainToml = pkgs.rust-bin.fromRustupToolchainFile ../rust-toolchain.toml;
-  cargoLockFile = conduit + /Cargo.lock;
-
-  cargo = rustToolchainToml;
-  rustc = rustToolchainToml;
-
-  buildInputs = [
-    pkgs.rocksdb
-  ];
-
-  nativeBuildInputs = [
-    pkgs.rustPlatform.bindgenHook
-  ];
-
-  devInputs = [
-    rustToolchainToml
-  ];
-
-  fmtInputs = [
-    pkgs.alejandra
-    pkgs.treefmt
-  ];
-  # TODO add meta
-in rec {
-  # crate2nix - better incremental builds, but uses ifd
-  #packages.conduit = crate2nixPkgs.callPackage ./crate2nix.nix {
-  #inherit
-  #name
-  #src
-  #crate2nix
-  #;
-  #};
-
-  # native nixpkgs support - keep supported
-  packages.conduit-native = (pkgs.makeRustPlatform {inherit cargo rustc;}).buildRustPackage {
-    inherit
-      src
-      name
-      buildInputs
-      nativeBuildInputs
-      ;
-    LIBCLANG_PATH = pkgs.lib.makeLibraryPath [pkgs.llvmPackages_latest.libclang.lib];
-    cargoLock = {
-      lockFile = src + "/Cargo.lock";
-      outputHashes = {
-        "heed-0.10.6" = "sha256-rm02pJ6wGYN4SsAbp85jBVHDQ5ITjZZd+79EC2ubRsY=";
-        "reqwest-0.11.9" = "sha256-wH/q7REnkz30ENBIK5Rlxnc1F6vOyuEANMHFmiVPaGw";
-        "ruma-0.4.0" = "sha256-gRhhGWpMiLeA8TNgBMsFEBky1TISr/GXNxLpTXLgUQE";
+    # native nixpkgs support - keep supported
+    packages.conduit-native = (pkgs.makeRustPlatform {inherit cargo rustc;}).buildRustPackage {
+      inherit
+        src
+        name
+        buildInputs
+        nativeBuildInputs
+        ;
+      LIBCLANG_PATH = pkgs.lib.makeLibraryPath [pkgs.llvmPackages_latest.libclang.lib];
+      cargoLock = {
+        lockFile = src + "/Cargo.lock";
+        outputHashes = {
+          "heed-0.10.6" = "sha256-rm02pJ6wGYN4SsAbp85jBVHDQ5ITjZZd+79EC2ubRsY=";
+          "reqwest-0.11.9" = "sha256-wH/q7REnkz30ENBIK5Rlxnc1F6vOyuEANMHFmiVPaGw";
+          "ruma-0.4.0" = "sha256-gRhhGWpMiLeA8TNgBMsFEBky1TISr/GXNxLpTXLgUQE";
+        };
       };
     };
-  };
 
-  defaultPackage = packages.conduit-native;
+    defaultPackage = packages.conduit-native;
 
-  # nix run
-  apps.conduit = flake-utils.lib.mkApp {drv = packages.conduit;};
-  defaultApp = apps.conduit;
+    # nix run
+    apps.conduit = flake-utils.lib.mkApp {drv = packages.conduit;};
+    defaultApp = apps.conduit;
 
-  devShells = {
-    conduit = pkgs.callPackage ./devShell.nix {
-      inherit buildInputs;
-      nativeBuildInputs = nativeBuildInputs ++ devInputs ++ fmtInputs;
+    devShells = {
+      conduit = pkgs.callPackage ./devShell.nix {
+        inherit buildInputs;
+        nativeBuildInputs = nativeBuildInputs ++ devInputs ++ fmtInputs;
+      };
+      fmtShell = pkgs.mkShell {
+        name = "fmt-shell";
+        nativeBuildInputs = fmtInputs;
+      };
     };
-    fmtShell = pkgs.mkShell {
-      name = "fmt-shell";
-      nativeBuildInputs = fmtInputs;
-    };
-  };
 
-  devShell = devShells.conduit;
-})
+    devShell = devShells.conduit;
+
+    overlays = {
+      default = final: prev: rec {
+        conduit = packages.${prev.system}.conduit;
+      };
+      nightly = final: prev: rec {
+        conduit-nightly = packages.${prev.system}.conduit;
+      };
+    };
+  }
+)
